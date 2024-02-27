10b6bdd159ec873a7b625b34e07ae1ae67d670ee
peterlimg <peterlimg@protonmail.com>
2024-02-27 10:45:47 +0800

    MPT cache minor refactor

--- core/statecache/statecache.go
+++ core/statecache/statecache.go
@@ -1,8 +1,6 @@
 package statecache
 
 import (
-	"sync"
-
 	lru "github.com/hashicorp/golang-lru"
 )
 
@@ -54,12 +52,9 @@ type valueNode struct {
 }
 
 type StateCache struct {
-	mu              sync.RWMutex
-	lastBreakRound  int64
-	lastCommitRound int64
-	// cache map[string]map[string]valueNode
-	cache     *lru.Cache
-	hashCache *lru.Cache
+	maxHisDepth int
+	cache       *lru.Cache
+	hashCache   *lru.Cache
 }
 
 func NewStateCache() *StateCache {
@@ -68,28 +63,20 @@ func NewStateCache() *StateCache {
 		panic(err)
 	}
 
-	hCache, err := lru.New(100)
+	maxHisDepth := 100
+	hCache, err := lru.New(maxHisDepth)
 	if err != nil {
 		panic(err)
 	}
 
 	return &StateCache{
-		// cache: make(map[string]map[string]valueNode),
-		cache:     cache,
-		hashCache: hCache,
+		maxHisDepth: maxHisDepth,
+		cache:       cache,
+		hashCache:   hCache,
 	}
 }
 
 func (sc *StateCache) commitRound(round int64, prevHash, blockHash string) {
-	sc.mu.Lock()
-	// update the lastCommitRound and lastBreakRound only when round is greater than lastCommitRound
-	// this is because the commitRound could happen for old rounds
-	if round > sc.lastCommitRound && sc.lastCommitRound+1 != round {
-		sc.lastBreakRound = round // any valueNode with Round < lastBreakRound is stale
-		sc.lastCommitRound = round
-	}
-
-	sc.mu.Unlock()
 	sc.hashCache.Add(blockHash, prevHash)
 }
 
@@ -130,8 +117,8 @@ func (sc *StateCache) Get(key, blockHash string) (Value, bool) {
 		blockHash = prevHash.(string)
 		vv, ok = bvs.Get(blockHash)
 		if !ok {
-			// break if the value is not found in previous 100 rounds
-			if count >= 100 {
+			// stop if the value is not found in previous maxHisDepth rounds
+			if count >= sc.maxHisDepth {
 				return nil, false
 			}
 
@@ -139,14 +126,6 @@ func (sc *StateCache) Get(key, blockHash string) (Value, bool) {
 		}
 
 		v := vv.(valueNode)
-		sc.mu.Lock()
-		if v.round < sc.lastBreakRound {
-			sc.mu.Unlock()
-			// break if value round is less than last break round
-			return nil, false
-		}
-		sc.mu.Unlock()
-
 		if v.deleted {
 			return nil, false
 		}
@@ -154,65 +133,6 @@ func (sc *StateCache) Get(key, blockHash string) (Value, bool) {
 		return v.data.Clone(), true
 	}
 }
-
-// func (sc *StateCache) getValue(key, blockHash string) (valueNode, bool) {
-// 	// sc.mu.RLock()
-// 	// defer sc.mu.RUnlock()
-
-// 	blockValues, ok := sc.cache.Get(key)
-// 	if !ok {
-// 		return valueNode{}, false
-// 	}
-
-// 	vv, ok := blockValues.(*lru.Cache).Get(blockHash)
-// 	v := vv.(valueNode)
-// 	if ok && !v.deleted {
-// 		v.data = v.data.Clone()
-// 		return v, true
-// 	}
-// 	return valueNode{}, false
-// }
-
-// shift copy the value from previous block to current
-// func (sc *StateCache) shift(prevHash, blockHash string) {
-// 	if prevHash == "" || blockHash == "" {
-// 		return
-// 	}
-// 	tm := time.Now()
-
-// 	keys := sc.cache.Keys()
-// 	for _, key := range keys {
-// 		blockValues, ok := sc.cache.Get(key)
-// 		if ok {
-// 			bvs := blockValues.(*lru.Cache)
-// 			vv, ok := bvs.Get(prevHash)
-// 			if ok {
-// 				// shift the value from previous block when it does not exist in present block
-// 				if _, exist := bvs.Get(blockHash); !exist {
-// 					v := vv.(valueNode)
-// 					v.data = v.data.Clone()
-// 					bvs.Add(blockHash, v)
-// 					sc.cache.Add(key, bvs)
-// 				}
-// 			}
-// 		}
-// 	}
-
-// for key, blockValues := range sc.cache {
-// 	v, ok := blockValues[prevHash]
-// 	if ok {
-// 		if _, exists := blockValues[blockHash]; !exists {
-// 			if sc.cache[key] == nil {
-// 				sc.cache[key] = make(map[string]valueNode)
-// 			}
-// 			v.data = v.data.Clone()
-// 			sc.cache[key][blockHash] = v
-// 		}
-// 	}
-// }
-
-// logging.Logger.Debug("state cache - shift", zap.Any("duration", time.Since(tm)))
-// }
 
 // Remove removes the values map with the given key
 func (sc *StateCache) Remove(key string) {
@@ -222,76 +142,3 @@ func (sc *StateCache) Remove(key string) {
 	sc.cache.Remove(key)
 	// delete(sc.cache, key)
 }
-
-// PruneRoundBelow removes all values that are below the given round
-// func (sc *StateCache) PruneRoundBelow(round int64) {
-// 	sc.mu.Lock()
-// 	defer sc.mu.Unlock()
-
-// 	var count int
-// 	for key, blockValues := range sc.cache {
-// 		for blockHash, value := range blockValues {
-// 			if value.round < round {
-// 				count++
-// 				delete(blockValues, blockHash)
-// 			}
-// 		}
-
-// 		// Delete the map if it becomes empty
-// 		if len(blockValues) == 0 {
-// 			delete(sc.cache, key)
-// 		}
-// 	}
-
-// 	logging.Logger.Debug("state cache - prune_round_below", zap.Int64("round", round), zap.Int("count", count))
-// }
-
-// PrettyPrint prints the state cache in a pretty format
-// func (sc *StateCache) PrettyPrint() {
-// 	sc.mu.RLock()
-// 	defer sc.mu.RUnlock()
-
-// 	// Sort keys in alphabetical order
-// 	var keys []string
-// 	for key := range sc.cache {
-// 		keys = append(keys, key)
-// 	}
-// 	sort.Strings(keys)
-
-// 	// Print values for each key
-// 	for _, key := range keys {
-// 		fmt.Printf("Key: %s\n", key)
-
-// 		blockValues := sc.cache[key]
-
-// 		// Sort block hashes by round number in descending order
-// 		var rounds []int64
-// 		for _, value := range blockValues {
-// 			rounds = append(rounds, value.round)
-// 		}
-// 		sort.Slice(rounds, func(i, j int) bool {
-// 			return rounds[i] > rounds[j]
-// 		})
-
-// 		// Print values for each round
-// 		for _, round := range rounds {
-// 			fmt.Printf("  Round: %d\n", round)
-
-// 			// Sort block hashes for the same round
-// 			var hashes []string
-// 			for hash, value := range blockValues {
-// 				if value.round == round {
-// 					hashes = append(hashes, hash)
-// 				}
-// 			}
-// 			sort.Strings(hashes)
-
-// 			// Print values for each hash
-// 			for _, hash := range hashes {
-// 				value := blockValues[hash]
-// 				fmt.Printf("    Hash: %s\n", hash)
-// 				fmt.Printf("      Deleted: %v\n", value.deleted)
-// 			}
-// 		}
-// 	}
-// }
