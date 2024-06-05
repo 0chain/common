3d2ca83045d2a5fa924e731230f951c854485c06
peterlimg <peterlimg@protonmail.com>
2024-06-05 15:41:45 +1000

    Make proof and verify

--- core/verkletrie/verkletrie.go
+++ core/verkletrie/verkletrie.go
@@ -154,7 +154,7 @@ func (m *VerkleTrie) Commit() Hash {
 	return m.root.Commit().Bytes()
 }
 
-func (m *VerkleTrie) CommitAndFlush() (Hash, error) {
+func (m *VerkleTrie) Flush() (Hash, error) {
 	root, ok := m.root.(*verkle.InternalNode)
 	if !ok {
 		return Hash{}, errors.New("unexpected root node type")
@@ -190,16 +190,6 @@ func (m *VerkleTrie) CommitAndFlush() (Hash, error) {
 	return m.Hash(), nil
 }
 
-// func (m *VerkleTrie) Flush() error {
-// 	v, err := m.root.Serialize()
-// 	if err != nil {
-// 		return Hash{}, fmt.Errorf("serializing root node: %s", err)
-// 	}
-// 	if err := batch.Put(DBKey(m.key), v); err != nil {
-// 		return Hash{}, fmt.Errorf("put root node to disk: %s", err)
-// 	}
-// }
-
 type Keylist [][]byte
 
 func MakeProof(trie *VerkleTrie, keys Keylist) (*verkle.VerkleProof, verkle.StateDiff, error) {
@@ -211,23 +201,73 @@ func MakeProof(trie *VerkleTrie, keys Keylist) (*verkle.VerkleProof, verkle.Stat
 	return verkle.SerializeProof(proof)
 }
 
-func VerifyProof(vp *verkle.VerkleProof, stateDiff verkle.StateDiff, stateRoot []byte, keys Keylist) error {
+func verifyProof(vp *verkle.VerkleProof, stateDiff verkle.StateDiff, stateRoot []byte) (*verkle.Proof, error) {
 	dproof, err := verkle.DeserializeProof(vp, stateDiff)
 	if err != nil {
-		return fmt.Errorf("verkle proof deserialization error: %w", err)
+		return nil, fmt.Errorf("verkle proof deserialization error: %w", err)
 	}
 
 	root := new(verkle.Point)
 	if err := root.SetBytes(stateRoot); err != nil {
-		return fmt.Errorf("verkle root deserialization error: %w", err)
+		return nil, fmt.Errorf("verkle root deserialization error: %w", err)
 	}
 
 	tree, err := verkle.PreStateTreeFromProof(dproof, root)
 	if err != nil {
-		return fmt.Errorf("error rebuilding the pre-tree from proof: %w", err)
+		return nil, fmt.Errorf("error rebuilding the pre-tree from proof: %w", err)
 	}
 
-	return verkle.VerifyVerkleProofWithPreState(dproof, tree)
+	if err := verkle.VerifyVerkleProofWithPreState(dproof, tree); err != nil {
+		return nil, fmt.Errorf("verkle proof verification error: %w", err)
+	}
+
+	return dproof, nil
+}
+
+// VerifyProofPresence verifies that the verkle proof is valid and keys are presence in the state tree
+func VerifyProofPresence(vp *verkle.VerkleProof, stateDiff verkle.StateDiff, stateRoot []byte, keys Keylist) error {
+	if _, err := verifyProof(vp, stateDiff, stateRoot); err != nil {
+		return err
+	}
+
+	// v, _ := json.MarshalIndent(stateDiff, "", "  ")
+	// fmt.Println(string(v))
+
+	sdMap := make(map[string][32]byte, len(stateDiff))
+	for _, sd := range stateDiff {
+		for _, su := range sd.SuffixDiffs {
+			path := append(sd.Stem[:], su.Suffix)
+			if su.CurrentValue != nil {
+				sdMap[string(path)] = *su.CurrentValue
+			}
+		}
+	}
+
+	for _, key := range keys {
+		if _, ok := sdMap[string(key)]; !ok {
+			return fmt.Errorf("verkle proof could not find key: %x", key)
+		}
+	}
+
+	return nil
+}
+
+func VerifyProofAbsence(vp *verkle.VerkleProof, stateDiff verkle.StateDiff, stateRoot []byte, keys Keylist) error {
+	dproof, err := verifyProof(vp, stateDiff, stateRoot)
+	if err != nil {
+		return err
+	}
+
+	// v, _ := json.MarshalIndent(stateDiff, "", "  ")
+	// fmt.Println(string(v))
+	// fmt.Println(dproof.PreValues)
+
+	for _, v := range dproof.PreValues {
+		if len(v) != 0 {
+			return errors.New("verkle proof contains value")
+		}
+	}
+	return nil
 }
 
 func HexToBytes(s string) []byte {
--- core/verkletrie/verkletrie_test.go
+++ core/verkletrie/verkletrie_test.go
@@ -1,8 +1,11 @@
 package verkletrie
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 
+	"github.com/ethereum/go-verkle"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -10,6 +13,13 @@ var keys = [][]byte{
 	HexToBytes("a3b45d6e7f890123456789abcdef0123456789abcdef0123456789abcdef0123"),
 	HexToBytes("123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0"),
 	HexToBytes("abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789"),
+	HexToBytes("f10a5b26c19e3d94b6a87fe0c41269abd4e29a935fb7e4cd9a51f8b1272d3a68"),
+	HexToBytes("43e1b7c9f20d5e76b4a1c8f62e9a03d57b9c16e3fa12b7a589d20c4f7e5a8c3b"),
+	HexToBytes("e6d2a1f94c10b3e87f5d2b9c4a3ea62f709b52d68e13c4d7a8b61f9eb3c2475a"),
+	HexToBytes("a58c3e7b12f4d96a3b8e27f0a1c76e5d49f2b6a13d0c5e28f7b19d4a2c07b831"),
+	HexToBytes("f094e7a3b8d2c5f1093b4e76a8d2b5c4e1f7a3965d2c0e4b8a5d19f2b3c7e8b4"),
+	HexToBytes("1a9f4c2d3b6e7a5f8d29c3b14e07a6d5c8b3f2d7a1e4b9c5d702b8f1c3a9d4e6"),
+	HexToBytes("7e8c3b2f6a1d5e0c8b29f4a713d6e5c8f7a2b1d0e9c5a4b8f36e7d12c8b0a4f9"),
 }
 
 func TestVerkleTrie_Insert(t *testing.T) {
@@ -67,7 +77,7 @@ func TestVerkleTrie_Commit(t *testing.T) {
 	assert.Nil(t, err)
 
 	// Commit the tree
-	vt.CommitAndFlush()
+	vt.Flush()
 
 	// fmt.Println(string(verkle.ToDot(vt.root)))
 
@@ -94,7 +104,7 @@ func TestTreeKeyStorage(t *testing.T) {
 	err := vt.Insert(key, rootHash)
 	assert.Nil(t, err)
 
-	vt.Commit()
+	vt.Flush()
 
 	v, err := vt.Get(key)
 	assert.Nil(t, err)
@@ -105,7 +115,7 @@ func TestTreeKeyStorage(t *testing.T) {
 	err = vt.InsertValue(filepathHash, bigValue)
 	assert.Nil(t, err)
 
-	vt.Commit()
+	vt.Flush()
 
 	vv, err := vt.GetValue(filepathHash)
 	assert.Nil(t, err)
@@ -129,10 +139,81 @@ func TestTreeStorageLargeData(t *testing.T) {
 	err := vt.InsertValue(filepathHash, values)
 	assert.Nil(t, err)
 
-	vt.Commit()
+	vt.Flush()
 
 	v, err := vt.GetValue(filepathHash)
 	assert.Nil(t, err)
 
 	assert.Equal(t, values, v)
+}
+
+func TestInsertsNodeChanges(t *testing.T) {
+	vt := New("alloc_1", NewInMemoryVerkleDB())
+	for i := 0; i < len(keys[:7]); i++ {
+		err := vt.InsertValue(keys[i], keys[i])
+		assert.Nil(t, err)
+	}
+
+	vt.Flush()
+
+	vt.Insert(keys[7], keys[7])
+	vt.Flush()
+}
+
+func TestProof(t *testing.T) {
+	vt := New("alloc_1", NewInMemoryVerkleDB())
+	for i := 0; i < len(keys[:3]); i++ {
+		err := vt.Insert(keys[i], keys[i])
+		assert.Nil(t, err)
+	}
+
+	root := vt.Commit()
+	dproof, stateDiff, err := MakeProof(vt, keys[:3])
+	assert.Nil(t, err)
+
+	dproofBytes, err := json.Marshal(dproof)
+	assert.Nil(t, err)
+
+	stateDiffBytes, err := json.Marshal(stateDiff)
+	assert.Nil(t, err)
+
+	// deserialize dproof
+	dproof2 := &verkle.VerkleProof{}
+	err = json.Unmarshal(dproofBytes, dproof2)
+	assert.Nil(t, err)
+
+	// deserialize stateDiff
+	stateDiff2 := verkle.StateDiff{}
+	err = json.Unmarshal(stateDiffBytes, &stateDiff2)
+	assert.Nil(t, err)
+
+	err = VerifyProofPresence(dproof2, stateDiff2, root[:], keys[:3])
+	assert.Nil(t, err)
+}
+
+func TestProofNotExistKey(t *testing.T) {
+	vt := New("alloc_1", NewInMemoryVerkleDB())
+	for i := 0; i < len(keys[:3]); i++ {
+		err := vt.Insert(keys[i], keys[i])
+		assert.Nil(t, err)
+	}
+
+	root := vt.Commit()
+
+	t.Run("proof no key exists", func(t *testing.T) {
+		dp, sdiff, err := MakeProof(vt, keys[3:])
+		assert.Nil(t, err)
+
+		err = VerifyProofAbsence(dp, sdiff, root[:], keys[3:])
+		fmt.Println("err:", err)
+		assert.Nil(t, err)
+	})
+
+	t.Run("proof absence of exist key - should fail", func(t *testing.T) {
+		dp, sdiff, err := MakeProof(vt, keys[2:])
+		assert.Nil(t, err)
+
+		err = VerifyProofAbsence(dp, sdiff, root[:], keys[2:])
+		assert.EqualError(t, err, "verkle proof contains value")
+	})
 }
